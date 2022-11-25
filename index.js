c1f7acc9cc069c1123d97950e973f99545163f39
Fleur41 <Muriithikings456@gmail.com>
2022-11-25 11:26:07 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const receivesAFunction = (spy) => {
+    returnsANamedFunction();
+    return spy();
+
+};
+
+const returnsANamedFunction = function() {
+    return function a() {
+        1 + 1;
+    };
+};
+
+const returnsAnAnonymousFunction = () => {
+    return function () {
+        1 + 1;
+    };
+};
+
